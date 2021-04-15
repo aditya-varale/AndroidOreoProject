405bc305433898c3ea7a0d67e2f0ef32a18572ca
ADITYA <varalemcoe@gmail.com>
2021-04-15 14:37:40 +0530

    Added Game Connect Game

--- app/src/main/java/com/example/learnandroidoreo/MainActivity.java
+++ app/src/main/java/com/example/learnandroidoreo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.learnandroidoreo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.learnandroidoreo.GameConnect.GameConnectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Introduction(View view) {
+    }
+
+    public void GameConnect(View view) {
+        Intent intent = new Intent(MainActivity.this, GameConnectActivity.class);
+        startActivity(intent);
     }
 }
